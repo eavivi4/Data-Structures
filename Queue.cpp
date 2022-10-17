6c18ccbcdafc288666a6238bf841363d034da361
eavivi4 <avivi.eden@gmail.com>
2022-10-16 18:01:58 -0800

    Added Priority Queue implementation

--- Queue.cpp
+++ Queue.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 class Node {
 public:
     int data;
@@ -74,8 +75,106 @@ public:
     }
 };
 
-class PriorityQueue{
+class PriorityNode {
+public:
+    int data;
+    int priority;
+    PriorityNode* next;
 
+    PriorityNode(int value, int rank) {
+        data = value;
+        priority = rank;
+        next = nullptr;
+    }
+};
+
+class PriorityQueue{
+    PriorityNode* front;
+    PriorityNode* back;
+public:
+    PriorityQueue()
+    {
+        front = nullptr;
+        back = nullptr;
+    }
+    int getFront() {
+        return front->data;
+    }
+
+    int getBack() {
+        return back->data;
+    }
+
+    void push(int value, int priority) {
+        PriorityNode* newNode = new PriorityNode(value, priority);
+        PriorityNode* temp = front;
+        if (front == nullptr)
+        {
+            // If queue is empty
+            front = newNode;
+            back = newNode;
+            return;
+        }
+
+        if (front->priority > priority)
+        {
+            // Insert before front
+            temp = front;
+            front = newNode;
+            front->next = temp;
+
+            return;
+        }
+        
+        PriorityNode* curr = front;
+        while (curr->next != nullptr)
+        {
+            if (curr->next->priority > priority)
+            {
+                // Insert before next element
+                temp = curr->next;
+                curr->next = newNode;
+                newNode->next = temp;
+                
+                return;
+            }
+            else
+            {
+                // Continue to next element in queue
+                curr = curr->next;
+            }
+        }
+
+        // Add last element
+        curr->next = newNode;
+        back = newNode;
+        return;
+    }
+
+    void pop() {
+        
+        // Pops the element with the most priority, which is in the front
+        if (!front)
+        {
+            // List is empty
+            return;
+        }
+        PriorityNode* curr = front;
+        front = curr->next;
+        delete curr;
+        return;
+    }
+
+    void print() {
+        PriorityNode* curr = front;
+        while (curr != nullptr)
+        {
+            cout << "#" << curr->priority << " " << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << endl;
+        return;
+    }
 };
 
 int main() {
@@ -104,5 +203,31 @@ int main() {
     q->push(2);
     q->print();
     cout << "Front: " << q->getFront() << " Back: " << q->getBack() << endl;
+
+    cout << "Priority Queue: " << endl;
+    PriorityQueue* pq = new PriorityQueue();
+    pq->push(1,4);
+    pq->print();
+    pq->push(2,3);
+    pq->print();
+    pq->push(3,1);
+    pq->print();
+    pq->push(4,2);
+    pq->print();
+    cout << "Front: " << pq->getFront() << " Back: " << pq->getBack() << endl;
+    pq->push(5,8);
+    pq->print();
+    pq->push(6,6);
+    pq->print();
+    pq->pop();
+    pq->print();
+    cout << "Front: " << pq->getFront() << " Back: " << pq->getBack() << endl;
+    pq->pop();
+    pq->print();
+    pq->push(1,1);
+    pq->print();
+    pq->push(2,5);
+    pq->print();
+    cout << "Front: " << pq->getFront() << " Back: " << pq->getBack() << endl;
     return 0;
 }
